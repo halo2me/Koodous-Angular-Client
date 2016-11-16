3fc480b872be8289126ffdc3ff65bc358bd22830
Daniel Vaca <dvaca@hispasec.com>
2016-11-16 18:03:16 +0100

    simplified way to deploy the app

--- compile.sh
+++ compile.sh
@@ -1,10 +1,7 @@
+npm install -g yuglify
+mkdir compiled
 yuglify assets/css/*.css -c compiled/styles
 yuglify app/*.js app/**/*.js app/**/**/*.js -c compiled/app
-
-# <script src="assets/js/codemirror.js"></script>
-# <script src="assets/js/codemirror_yara.js"></script>
-# <script src="assets/js/moment-with-locales.js"></script>
-# <script src="assets/js/qrcode.min.js"></script>
-
 yuglify assets/js/codemirror.js assets/js/codemirror_yara.js assets/js/moment-with-locales.js assets/js/qrcode.min.js -c compiled/assets
 yuglify assets/js/mentio.min.js assets/js/angular*.js assets/js/ui*.js -c compiled/angular-assets
+cp index_prod.html compiled/index.html
