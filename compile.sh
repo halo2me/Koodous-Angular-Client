479fb426b2146a620d76d0caa59874daff22bda9
Daniel Vaca <dvaca@hispasec.com>
2016-11-16 18:06:55 +0100

    deploy script minor fix

--- compile.sh
+++ compile.sh
@@ -4,4 +4,5 @@ yuglify assets/css/*.css -c compiled/styles
 yuglify app/*.js app/**/*.js app/**/**/*.js -c compiled/app
 yuglify assets/js/codemirror.js assets/js/codemirror_yara.js assets/js/moment-with-locales.js assets/js/qrcode.min.js -c compiled/assets
 yuglify assets/js/mentio.min.js assets/js/angular*.js assets/js/ui*.js -c compiled/angular-assets
-cp index_prod.html compiled/index.html
+rm index.html
+cp index_prod.html index.html
